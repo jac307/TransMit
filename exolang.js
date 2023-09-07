f44b59f25d81dbef2e7df57f40e55dc6da001816
Jessica Rodriguez <rodrij28@mcmaster.ca>
2023-09-07 15:03:03 -0400

    editing exolang.js

--- exolang.js
+++ exolang.js
@@ -8,9 +8,17 @@ TransMit.prototype.evaluate = function(zone, text) {
   return T.evaluate(this.tm)(txt)();
 }
 
-TransMit.prototype.animate = function(zone) {
+TransMit.prototype.clearZone = function(zone) {}
+
+TransMit.prototype.setTempo = function(foreignTempo) {}
+
+TransMit.prototype.preAnimate = function() {}
+
+TransMit.prototype.animateZone = function(zone) {
   return T.animate(this.tm)();
 }
+
+TransMit.prototype.postAnimate = function() {}
 
 export function exoLang(canvas) {
   return new TransMit(canvas);
